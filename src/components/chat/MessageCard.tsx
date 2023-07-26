52525785242742dd5e036e9bddd96dd77a272915
TheNozomi <13172293+TheNozomi@users.noreply.github.com>
2023-07-26 17:35:12 -0300

    fix time input

--- src/components/chat/MessageCard.tsx
+++ src/components/chat/MessageCard.tsx
@@ -1,13 +1,14 @@
 import { Button, Card, Group, NativeSelect, Stack, TextInput, Textarea } from '@mantine/core';
 import { TimeInput } from '@mantine/dates';
 import { useEffect, useState } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
 import data from '../../data/members.json';
 import { IChatEntry, Member } from '../../types';
 
 export function MessageCard({ message, onUpdate }: MessageCardProps) {
   const [member, setMember] = useState<string>(message.member.name.toLowerCase());
-  const [time, setTime] = useState<Date>(message.timestamp);
-  const [timeString] = useState<string>('');
+  const [time, setTime] = useState<Dayjs>(dayjs(message.timestamp));
+  const [timeString, setTimeString] = useState<string>('');
   const [content, setContent] = useState(message.content);
   const formMembers = data.members.map((_member) => ({
     value: _member.name.toLowerCase(),
@@ -25,16 +26,14 @@ export function MessageCard({ message, onUpdate }: MessageCardProps) {
   }, [member, content]);
 
   const updateTime = (value: string) => {
-    console.log(value);
     const [hours, minutes] = value.split(':');
 
-    const newTime = time;
+    const newTime = time
+      .set('hour', parseInt(hours, 10))
+      .set('minute', parseInt(minutes, 10));
 
-    newTime.setHours(parseInt(hours, 10));
-    newTime.setMinutes(parseInt(minutes, 10));
-
-    console.log(newTime);
     setTime(newTime);
+    setTimeString(newTime.format('HH:mm'));
   };
 
   return (
@@ -53,7 +52,6 @@ export function MessageCard({ message, onUpdate }: MessageCardProps) {
               placeholder="Hora"
               value={timeString}
               onChange={(e) => updateTime(e.target.value)}
-              withSeconds
             />
           </Group>
         </Card.Section>
